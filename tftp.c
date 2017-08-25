ffbea6721f4206c7d767451a869e7c370319e133
mayfa <pavel.akira.marek@gmail.com>
2017-08-25 17:56:32 +0200

    Fix filename and modename extracting.

--- tftp.c
+++ tftp.c
@@ -16,6 +16,7 @@ void
 read_packet(const uint8_t *packet, int packet_len, tftp_header_t *hdr)
 {
 	int i = 2, file_idx = 0, mode_idx = 0;
+	char modename[MODENAME_LEN];
 
 	/* First two bytes are opcode. */
 	if (packet[0] != 0) {
@@ -29,19 +30,13 @@ read_packet(const uint8_t *packet, int packet_len, tftp_header_t *hdr)
 	case RRQ_OPCODE:
 	case WRQ_OPCODE:
 		/* Extract filename. */
-		while (packet[i] != '\0') {
-			filename[file_idx++] = packet[i];
-			++i;
-		}
+		strcpy(filename, packet[i]);
 		hdr->req_filename = filename;
 
-		++i;
+		i += strlen(packet[i]);
 
 		/* Extract modename. */
-		while (packet[i] != '\0') {
-			modename[mode_idx++] = packet[i];
-			++i;
-		}
+		strcpy(modename, packet[i]);
 		hdr->req_mode = modename;
 
 		break;
