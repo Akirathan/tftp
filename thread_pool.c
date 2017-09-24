2c07d45302cf44303d1e6391926cdb4f9a81a627
mayfa <pavel.akira.marek@gmail.com>
2017-09-24 11:05:28 +0200

    Major change to thread_pool.
    Thread pool reuses threads, not creates new ones.

--- thread_pool.c
+++ thread_pool.c
@@ -8,102 +8,84 @@
 
 #include "thread_pool.h"
 
-static struct {
-	pthread_mutex_t mutex;
-	pthread_t thread;
-	int active;
-	/*
-	 * Dynamicaly allocated buffer for parameters passed to the thread.
-	 * Note that it is important to copy parameters into this buffer,
-	 * otherwise pointer to "volatile" value would be passed to the
-	 * thread routine (value would be modified in generic_server function
-	 * in server.c).
-	 */
-	void *par_buf;
-	size_t par_len;
-} threads[THREAD_NUM];
 
-/**
- * This function must be pushed as cleanup handler to every thread that uses
- * this thread_pool.
- */
+static void * thread_routine(void *arg);
+
+thread_t threads[THREAD_NUM];
+static pthread_barrier_t barrier;
+
 void
-remove_thread(void *arg)
+pool_destroy()
 {
-	for (size_t i = 0; i < THREAD_NUM; ++i) {
-		if (pthread_equal(pthread_self(), threads[i].thread)) {
-			pthread_mutex_lock(&threads[i].mutex);
-			/* pthread_t are reused because threads are detached - null it. */
-			memset(&threads[i].thread, 0, sizeof(threads[i].thread));
-			free(threads[i].par_buf);
-			threads[i].par_len = 0;
-			threads[i].active = 0;
-		}
-		pthread_mutex_unlock(&threads[i].mutex);
-	}
-}
-
-/**
- * Inserts new detached thread into pool.
- * init_pool must be called before this function.
- * If there is no room for new thread, waits for first one.
- */
-void
-new_thread(void * (* fnc) (void *), void *arg, size_t arg_len)
-{
-	void *p;
-	pthread_attr_t attr;
-
-	/* Init detached attribute. */
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-
-	/* Allocate memory for arguments. */
-	p = malloc(arg_len);
-	memcpy(p, arg, arg_len);
-
-	for (size_t i = 0; i < THREAD_NUM; ++i) {
-		if (threads[i].active)
-			continue;
-
-		pthread_mutex_lock(&threads[i].mutex);
-		threads[i].par_buf = p;
-		threads[i].par_len = arg_len;
-		/* Start detached thread. */
-		threads[i].active = 1;
-		pthread_mutex_unlock(&threads[i].mutex);
-		pthread_create(&threads[i].thread, &attr, fnc, threads[i].par_buf);
-		return;
-	}
-
-	/* All threads are active - wait for first one. */
-	for (;;) {
-		if (threads[0].active) {
-			sched_yield();
-		}
-		else {
-			break;
-		}
-	}
-
-	pthread_mutex_lock(&threads[0].mutex);
-	threads[0].par_buf = p;
-	threads[0].par_len = arg_len;
-	threads[0].active = 1;
-	pthread_mutex_unlock(&threads[0].mutex);
-	pthread_create(&threads[0].thread, NULL, fnc, threads[0].par_buf);
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        pthread_cancel(threads[i].pthread);
+        pthread_join(threads[i].pthread, NULL);
+    }
 }
 
 void
-init_pool()
+pool_init()
 {
-	for (size_t i = 0; i < THREAD_NUM; ++i) {
-		if (pthread_mutex_init(&threads[i].mutex, NULL) != 0) {
-			perror("pthread_mutex_init");
-			exit(EXIT_FAILURE);
-		}
-		threads[i].active = 0;
-		threads[i].par_len = 0;
-	}
+    pthread_barrier_init(&barrier, NULL, THREAD_NUM + 1);
+
+    for (size_t i = 0; i < THREAD_NUM; ++i) {
+        pthread_mutex_init(&threads[i].mtx, NULL);
+        threads[i].work_ready = false;
+        pthread_cond_init(&threads[i].work_signal, NULL);
+        pthread_create(&threads[i].pthread, NULL, thread_routine, threads + i);
+    }
+
+    /* Wait until every thread is initialized. */
+    pthread_barrier_wait(&barrier);
+}
+
+
+static void *
+thread_routine(void *arg) {
+    thread_t *this = (thread_t *) arg;
+
+    pthread_mutex_lock(&this->mtx);
+
+    /* Wait until every thread is initialized. */
+    pthread_barrier_wait(&barrier);
+
+    for (;;) {
+        /* Wait for work_signal. */
+        while (!this->work_ready) {
+            pthread_cond_wait(&this->work_signal, &this->mtx);
+        }
+        this->fnc(this->params);
+        /* Cleanup. */
+        free(this->params);
+        this->work_ready = false;
+    }
+    pthread_mutex_unlock(&this->mtx);
+    return NULL;
+}
+
+int
+pool_insert(void * (* fnc) (void *), void *arg, size_t arg_len)
+{
+    void *p;
+
+    // Allocate memory for arg.
+    p = malloc(arg_len);
+    memcpy(p, arg, arg_len);
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        /* Lock can be acquired just in the moment when thread is waiting for signal. */
+        if (!threads[i].work_ready && pthread_mutex_trylock(&threads[i].mtx) == 0) {
+            threads[i].fnc = fnc;
+            threads[i].params = p;
+            threads[i].work_ready = true;
+            //if (debug)
+            //    printf("Thread %lu signaled to work.\n", threads[i].pthread);
+            pthread_cond_signal(&threads[i].work_signal);
+            pthread_mutex_unlock(&threads[i].mtx);
+            return 0;
+        }
+    }
+
+    return EPOOL_FULL;
 }
 
--- thread_pool.h
+++ thread_pool.h
@@ -21,15 +21,29 @@
 
 /* Number of concurrently running threads. */
 #define THREAD_NUM		10
+#define EPOOL_FULL      1
 
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+#include <stdbool.h>
 
-void remove_thread(void *arg);
-void new_thread(void * (* fnc) (void *), void *arg, size_t arg_len);
-void init_pool();
+typedef struct _thread {
+    pthread_t pthread;
+    /**
+     * mtx is locked while this thread is working.
+     */
+    pthread_mutex_t mtx;
+    bool work_ready;
+    pthread_cond_t work_signal;
+    void * (* fnc) (void *);
+    void *params;
+} thread_t;
+
+void pool_init();
+void pool_destroy();
+int pool_insert(void * (* fnc) (void *), void *arg, size_t arg_len);
 
 #endif /* THREAD_POOL_H_ */
